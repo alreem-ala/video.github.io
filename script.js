df563bc9477d70fdf5c90fc490678084459f69e1
Alreem <aha9250@nyu.edu>
2023-12-12 07:23:16 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+alert("I am not a typical cinema webpage, prepare to interact!");
+
+
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
